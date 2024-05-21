77793477ceb03da7d635ae62b413ada3cbdd25be
Alvaro Condori <con21033@byui.edu>
2024-05-21 20:02:44 -0300

    mongodb and mongoose

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
